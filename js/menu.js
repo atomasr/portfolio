06b75c52135879ea055ae7d34f4c2eb4b5300e3e
Tomás Rodríguez <augustotomasrodriguez@gmail.com>
2024-06-25 01:03:03 -0300

    initial commit

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
