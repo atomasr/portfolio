c75f667145682cfdefe580e09e6cd6eb92c33a89
Tomás Rodríguez <augustotomasrodriguez@gmail.com>
2024-06-27 12:03:15 -0300

    close nav bar done

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,17 @@
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const navLinks = document.querySelectorAll(".nav-links");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', function(event) {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
+}
