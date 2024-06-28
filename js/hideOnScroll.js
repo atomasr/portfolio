f4db4142389eff391af4ab63ce6411f3ea7fc8c2
Tomás Rodríguez <augustotomasrodriguez@gmail.com>
2024-06-27 23:50:52 -0300

    no scroll while menu active mobile

--- js/hideOnScroll.js
+++ js/hideOnScroll.js
@@ -6,7 +6,7 @@ let hamburgerMenu = document.querySelector(".hamburger");
 window.onscroll = function() {
   let currentScrollPos = window.scrollY;
   console.log(currentScrollPos);
-  if ((Math.abs(prevScrollPos) > currentScrollPos)||(currentScrollPos < 100)||(hamburgerMenu.classList.contains("active"))) {
+  if ((Math.abs(prevScrollPos) > currentScrollPos)||(currentScrollPos < 50)||(hamburgerMenu.classList.contains("active"))) {
     document.querySelector("nav").classList.remove("hideNav");
     document.querySelector("footer").classList.remove("hideFooter");
   } else {
--- js/menu.js
+++ js/menu.js
@@ -7,6 +7,7 @@ const navLinks = document.querySelectorAll(".nav-links");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  body.classList.toggle("no-scroll");
 });
 
 
@@ -14,5 +15,6 @@ for (var i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener('click', function(event) {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+    body.classList.remove("no-scroll");
   });
 }
